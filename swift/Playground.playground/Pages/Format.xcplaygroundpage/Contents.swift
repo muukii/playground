b629222f6e229117575513ec3bef387d575605da
muukii <m@muukii.me>
2017-02-21 23:44:06 +0900

    Add Format

---
+++ swift/Playground.playground/Pages/Format.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+//: [Previous](@previous)
+
+import Foundation
+
+extension String {
+
+  public init(template: String, args: [String : CustomStringConvertible]) {
+
+    // 適当
+    var text = template
+
+    for arg in args {
+      let format = "{{ \(arg.key) }}"
+      assert(text.range(of: format) != nil, "Not found key : \(arg.key)")
+      text = text.replacingOccurrences(of: format, with: arg.value.description)
+    }
+
+    self = text
+  }
+
+  public func asTemplate(args: [String : CustomStringConvertible]) -> String {
+    return String(template: self, args: args)
+  }
+}
+
+let template = "hello {{ nickname }}, {{ age }}, {{ hight }}🙏"
+
+String(template: template, args: [
+  "nickname" : "muukii",
+  "age" : 1,
+  "hight" : 198.4,
+  ])
+
+template.asTemplate(args: [
+  "nickname" : "muukii",
+  "age" : 18,
+  "hight" : 198.4,
+  ])
+
+
+//: [Next](@next)
