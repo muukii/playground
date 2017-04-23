58d8e59c65c28858ea14740d91430f8fcc1a062c
muukii <m@muukii.me>
2017-04-23 16:23:50 +0900

    ⚗️

--- swift/Playground/main.playground/Pages/Error.xcplaygroundpage/Contents.swift
+++ swift/Playground/main.playground/Pages/Error.xcplaygroundpage/Contents.swift
@@ -8,11 +8,22 @@ enum Error: Swift.Error, LocalizedError {
   var errorDescription: String? {
     switch self {
     case .a:
-      return "hello"
+      return "hello-20345786304958740958704985670495867029485-30948510-94385-3094851290-438530-948510-394851390-4852390-485-239048529048523904852903845-2903485-29034alskdjg;as;kdfja;slkdfja;lskdfwerpoituweproituwperoituwperoituwperoituwpeorituwpeori"
     }
+  }
+  
+  var _domain: String {
+    return "jp"
+  }
+  
+  var failureReason: String? {
+    print("called reason")
+    return "asd"
   }
 }
 
-print(Error.a.localizedDescription)
+print((Error.a as NSError))
+
+NSError(domain: "jp.eure.pairs", code: 0, userInfo: [:])
 
 //: [Next](@next)
--- swift/RxPlayground/Playground.playground/Pages/Execute.xcplaygroundpage/Contents.swift
+++ swift/RxPlayground/Playground.playground/Pages/Execute.xcplaygroundpage/Contents.swift
@@ -8,6 +8,19 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+public class ExecuteToken {
+  
+  var disposable: Disposable?
+  
+  init() {
+    
+  }
+  
+  func cancel() {
+    disposable?.dispose()
+  }
+}
+
 extension ObservableType {
 
   /// Observableを実行
@@ -17,36 +30,65 @@ extension ObservableType {
   /// - Parameter disposeBag: このdisposeBagが存在するまで実行されます。
   /// - Returns: 結果をshareReplay(1)で渡します。
   @discardableResult
-  public func execute(_ disposeBag: DisposeBag) -> Observable<E> {
-
-    let s = shareReplay(1)
-    s.subscribe().addDisposableTo(disposeBag)
-
+  public func execute(_ disposeBag: DisposeBag, token: ExecuteToken) -> Observable<E> {
+    
+    let a = publish()
+    
+    let s = a.shareReplay(1)
+    s.subscribe().disposed(by: disposeBag)
+    
+    let d = a.connect()
+    token.disposable = d
+    d.disposed(by: disposeBag)
+    
     return s
   }
 }
 
 let o = Observable<Int>.create { o in
+  
+  print("began")
 
-  DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: { 
+  DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
 
+    print("perform")
     o.onNext(1)
     o.onCompleted()
   })
 
-  return Disposables.create()
+  return Disposables.create {
+    print("dispose")
+  }
 }
+.debug()
+
+let token = ExecuteToken()
 
 var serviceDisposeBag = DisposeBag()
 var viewModelDisposeBag = DisposeBag()
 
-o
+let s = o
+  .execute(serviceDisposeBag, token: token)
+
+s
   .debug()
-  .execute(serviceDisposeBag)
+  .subscribe()
+  .addDisposableTo(viewModelDisposeBag)
+
+s
+  .debug()
   .subscribe()
   .addDisposableTo(viewModelDisposeBag)
 
 viewModelDisposeBag = DisposeBag()
-serviceDisposeBag = DisposeBag()
+//serviceDisposeBag = DisposeBag()
+
+DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+  
+  token.cancel()
+})
+
+
+
 
 //: [Next](@next)
