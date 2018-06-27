6c228a197b6e8f7d49d0796e724715038b5027a4
Muukii <muukii.app@gmail.com>
2018-06-27 12:47:02 +0900

    :evergreen_tree: Update

--- swift/PromiseKit/TestDrive-PromiseKit.xcworkspace/Playground.playground/Contents.swift
+++ swift/PromiseKit/TestDrive-PromiseKit.xcworkspace/Playground.playground/Contents.swift
@@ -1,2 +1,28 @@
-import UIKit
+import Foundation
 
+import PromiseKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+//print(Thread.current)
+
+func run() -> Promise<Int> {
+
+  return
+    Promise<Int>.init { (r) in
+
+      DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: {
+
+        //    print(Thread.current)
+
+        r.fulfill(1)
+      })
+
+      }
+      .then { (v) -> Guarantee<Int> in
+        Guarantee<Int>.init(resolver: { (c) in
+          c(v + 1)
+        })
+  }
+}
