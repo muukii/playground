2a0181a500015178bb52de9679bd277ecd100dda
Muukii <muukii.app@gmail.com>
2019-01-24 21:23:01 +0900

    :evergreen_tree: Update

--- glsl/image_distrotion.glsl
+++ glsl/image_distrotion.glsl
@@ -12,7 +12,7 @@ uniform float u_time;
 void main() {
 
   vec2 uv = gl_FragCoord.xy / u_resolution;
-  uv.x += abs(pow(sin(u_time), 2.0) * 0.03) * sin(u_time * sin(uv.y) * 0.003);
+  uv.x += abs(pow(sin(u_time), 100.0) * 0.13) * sin(u_time * sin(uv.y) * 0.003);
   uv.y += abs(pow(sin(u_time), 2.0) * 0.03) * sin(u_time * sin(uv.x) * 0.003);
   vec4 color = texture2D(u_texture_0, uv);
   color.r *= 1.2;
